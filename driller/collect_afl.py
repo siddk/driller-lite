ecb1346b578861a6e0a6f1e11b1a291418d2af1f
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2017-08-08 11:00:41 -0400

    Update collect_afl

--- driller/collect_afl.py
+++ driller/collect_afl.py
@@ -5,26 +5,29 @@ Run the shellphuzz script on a given binary for the set amount of time, collecti
 """
 from subprocess import Popen
 import os
-import pickle
+import cPickle as pickle
 import sys
 import time
+import numpy as np
 
 TIMEOUT = 60 * 60 * 24      # Timeout (in seconds) => 24 hours
 COLLECT_EVERY = 60 * 3      # Collect every x seconds => 3 minutes
+NUM_PERSIST = 5000          # Number of AFL Persisted Samples => 5000
+SAMPLE_RATE = 2             # Sample Rate => 2 seconds
 
 
 def fuzz(bin_name):
     # Create Data Directory
-    if not os.path.exists('afl_data/afl_stats/%s' % bin_name):
-        os.mkdir('afl_data/afl_stats/%s' % bin_name)
+    if not os.path.exists('/vagrant/driller/afl_data/afl_stats/%s' % bin_name):
+        os.mkdir('/vagrant/driller/afl_data/afl_stats/%s' % bin_name)
 
     # Start Statistics
-    n_queue, data = 1, []                  # AFL Queue starts with 1 meaningful input (seed)
+    n_queue, data = 1, []                   # AFL Queue starts with 1 meaningful input (seed)
     start_time, counter = time.time(), 0    # Start time (in seconds)
 
     # Launch Shellphuzz Script
     process = Popen(['./shellphuzz', '-c', '1', '-t', str(TIMEOUT + 10),
-                     'data/linux/processed-challenges/%s/bin/%s' % (bin_name, bin_name)])
+                     '/vagrant/driller/data/linux/processed-challenges/%s/bin/%s' % (bin_name, bin_name)])
 
     # Prime Pump
     time.sleep(10)
@@ -44,19 +47,28 @@ def fuzz(bin_name):
             # Append items to data
             data.append((counter, obs, num_items - n_queue))
 
+            # Persist Loop:
+            sampled_inputs = []
+            for i in range(0, COLLECT_EVERY, 2):
+                f_name = np.random.choice(NUM_PERSIST)
+                with open('/dev/shm/work/%s/sync/fuzzer-master/persist_%d' % (bin_name, f_name), 'rb') as f:
+                    sampled_inputs.append(f.read())
+                time.sleep(SAMPLE_RATE)
+
+            # Dump Persisted Inputs
+            with open('/vagrant/driller/afl_data/afl_stats/%s/%s_persist.pik' % (bin_name, str(counter)), 'w') as g:
+                pickle.dump(sampled_inputs, g)
+
             # Update Book-Keeping
             n_queue = num_items
             counter += COLLECT_EVERY
-
-            # Sleep
-            time.sleep(COLLECT_EVERY)
 
     finally:
         # Terminate the process
         process.terminate()
 
         # Write Data to file
-        with open('afl_data/afl_stats/%s/%s.pik' % (bin_name, bin_name), 'w') as f:
+        with open('/vagrant/driller/afl_data/afl_stats/%s/%s.pik' % (bin_name, bin_name), 'w') as f:
             pickle.dump(data, f)
 
 
